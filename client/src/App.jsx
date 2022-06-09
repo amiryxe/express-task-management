4e4d4e6240752bc14ea3522e944b593eca661705
Amir Salehi <nik.amirsa@gmail.com>
2022-06-09 19:58:34 +0430

    create header component and include it to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
 
+import Header from './components/layouts/Header'
 import TaskList from './components/Tasks/TaskList'
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <TaskList />
     </div>
   )
