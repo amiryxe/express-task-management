b3a2b007cca42bbcd394ea51bcbf53d3ed860955
Amir Salehi <nik.amirsa@gmail.com>
2022-06-09 20:10:13 +0430

    style: task item

--- client/src/components/Tasks/TaskItem.jsx
+++ client/src/components/Tasks/TaskItem.jsx
@@ -1,7 +1,11 @@
 export default function TaskItem() {
     return (
-        <div className="tasks__item">
-            <h3>task title</h3>
+        <div className="tasks__item tasks__item--done">
+            <header>
+                <h3>task title</h3>
+                <span>(completed)</span>
+            </header>
+            
             <button>toggle</button>
         </div>
     )
