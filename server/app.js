b369d8e9c8896cd7083b59a83c912b1430b882b1
Amir Salehi <nik.amirsa@gmail.com>
2022-06-10 21:28:13 +0430

    create basic middleware to listen server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.route('/')
+    .get((req, res) => {
+        res.send('hello from server')
+    })
+
+app.listen(5000, () => {
+    console.log('Server run on http://localhost:5000')
+})
