9b70ba0fcc37f238da90bbd544b8e9bb2fde545a
Amir Salehi <nik.amirsa@gmail.com>
2022-06-11 07:49:49 +0430

    return main page message from server by route

--- server/app.js
+++ server/app.js
@@ -10,6 +10,10 @@ app.use(express.json())
 
 app.use('/api', api)
 
+app.get('/', (req, res) => {
+    res.send('<h1>Server main page</h1>')
+})
+
 app.listen(5000, () => {
     console.log('Server run on http://localhost:5000')
 })
