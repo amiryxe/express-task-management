44a3a15643c00283672c554699064456fd07dfc0
Amir Salehi <nik.amirsa@gmail.com>
2022-06-19 17:38:02 +0430

    style: task list

--- client/src/components/Tasks/TaskList.jsx
+++ client/src/components/Tasks/TaskList.jsx
@@ -4,6 +4,9 @@ export default function TaskList() {
     return (
         <>
             <TaskItem />
+            <TaskItem />
+            <TaskItem />
+            <TaskItem />
         </>
     )
 }
