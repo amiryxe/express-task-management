64ccade6477f7e1ab787ebb59c7801d1c771d973
Amir Salehi <nik.amirsa@gmail.com>
2022-06-20 15:36:44 +0430

    add description and update styles

--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <div className="header">
             <h1>Express task management</h1>
+            <p>Practical project by node.js and express to manage tasks</p>
         </div>
     )
 }
