08890985d82b25e5372fc7dc493808bf8ef3c12f
Amir Salehi <nik.amirsa@gmail.com>
2022-06-20 14:43:36 +0430

    create add task method in server by post method

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,7 +6,8 @@ const api = express.Router()
 
 api.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept')
+    res.setHeader('Content-Type', 'application/json')
     next()
 })
 
@@ -17,6 +18,16 @@ api.get('/tasks', (req, res) => {
         res.send(JSON.stringify(tasks))
     } else {
         res.status(400).send('no any task')
+    }
+})
+
+api.post('/tasks', (req, res) => {
+    try {
+        const task = new Task(req.body.title, req.body.completed)
+        task.save()
+        res.send(task)
+    } catch (err) {
+        res.status(400).send(err.message)
     }
 })
 
