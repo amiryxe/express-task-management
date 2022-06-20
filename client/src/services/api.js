693bab8550150596967ce04d0cfc2745309aa69e
Amir Salehi <nik.amirsa@gmail.com>
2022-06-20 12:33:39 +0430

    create req service for add task

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,11 +3,23 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const mainApi = createApi({
     reducerPath: 'mainApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+    tagTypes: ['tasks'],
     endpoints: (builder) => ({
         getTasks: builder.query({
-            query: () => 'tasks'
-        })
+            query: () => 'tasks',
+            providesTags: [{ type: "Tasks", id: "LIST" }],
+        }),
+        addTask: builder.mutation({
+            query(body) {
+                return {
+                    url: `tasks`,
+                    method: "POST",
+                    body
+                };
+            },
+            invalidatesTags: [{ type: "Tasks", id: "LIST" }],
+        }),
     }),
 })
 
-export const { useGetTasksQuery } = mainApi
+export const { useGetTasksQuery, useAddTaskMutation } = mainApi
