fea4e64189c642a390a854908abd97846554231d
Amir Salehi <nik.amirsa@gmail.com>
2022-07-03 08:30:09 +0430

    set in dynamically className to task item card

--- client/src/components/Tasks/TaskItem.jsx
+++ client/src/components/Tasks/TaskItem.jsx
@@ -1,16 +1,20 @@
-import CheckFill from '../../assets/images/check-fill.svg'
+import CheckFill from "../../assets/images/check-fill.svg"
 
 export default function TaskItem(data) {
-    const { title, completed } = data.data;
+  const { title, completed } = data.data
 
-    return (
-        <div className="tasks__item tasks__item--done">
-            <button>
-                <img src={CheckFill} alt="" />
-            </button>
+  return (
+    <div
+      className={`tasks__item ${
+        completed ? "tasks__item--done" : "tasks__item--inprogress"
+      }`}
+    >
+      <button>
+        <img src={CheckFill} alt="" />
+      </button>
 
-            <h3>{title}</h3>
-            <span>({completed ? 'completed' : 'in progress'})</span>
-        </div>
-    )
+      <h3>{title}</h3>
+      <span>({completed ? "completed" : "in progress"})</span>
+    </div>
+  )
 }
